9afe719a2fcce17c71ec9b9c6c659315da99e209
Devine Lu Linvega <aliceffekt@gmail.com>
2018-04-26 11:36:37 +1200

    Implementing input

--- desktop/sources/scripts/client.js
+++ desktop/sources/scripts/client.js
@@ -1,6 +1,7 @@
 function Client()
 {
   this.controller = new Controller();
+  this.input_el = null;
 
   this.start = function()
   {
@@ -11,7 +12,22 @@ function Client()
     this.controller.add("default","*","Documentation",() => { left.controller.docs(); },"CmdOrCtrl+Esc");
     this.controller.add("default","*","Reset",() => { left.theme.reset(); },"CmdOrCtrl+Backspace");
     this.controller.add("default","*","Quit",() => { left.project.quit(); },"CmdOrCtrl+Q");
-    
+
     this.controller.commit();
+
+    this.input_el = document.getElementById("input");
+    this.input_el.oninput = (key) => { this.hint(key); };
+    this.input_el.onkeyup = (key) => { if(key.key == "Enter"){ this.validate(); } };
+  }
+
+  this.hint = function(key)
+  {
+    console.log("HEY",key); 
+  }
+
+  this.validate = function(value = this.input_el.value)
+  {
+    this.input_el.value = "";
+    console.log("VALIDATE",value); 
   }
 }
