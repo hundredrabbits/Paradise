10f564883c5d860152184aa51e4ede77d8e46466
Devine Lu Linvega <aliceffekt@gmail.com>
2018-05-02 10:12:01 +1200

    *

--- desktop/server/actions/inventory.js
+++ desktop/server/actions/inventory.js
@@ -1,23 +1,25 @@
 function Inventory(host)
 {
   require(`../action`).call(this,host,"inventory");
+  
   this.docs = "View the contents of your inventory."
-    this.operate = function () {
-        let children = this.host.children();
-        if(children.length == 0){
-            return `<p>You are not carrying any vessels.</p>`;
-        }
-        let r = `<p>You are carrying the following vessels: `;
-        for (i in children) {
-            let vessel = children[i];
-            r += `<action data='drop the ${vessel.name()}'>${vessel.name()}</action>`;
-            if(children.length-1 != i){
-                r+=`, `;
-            }
-        }
-        r += `</p>`
-        return r;
+
+  this.operate = function () {
+    let children = this.host.children();
+    if(children.length == 0){
+      return `<p>You are not carrying any vessels.</p>`;
     }
+    let r = `<p>You are carrying the following vessels: `;
+    for (i in children) {
+      let vessel = children[i];
+      r += vessel.to_a();
+      if(children.length-1 != i){
+        r+=`, `;
+      }
+    }
+    r += `</p>`
+    return r;
+  }
 }
 
 module.exports = Inventory
