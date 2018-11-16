048d8cd424a93cb309631d93ac73c8843eb531a5
microlith57 <microlith57@gmail.com>
2018-11-17 07:12:36 +1300

    Convert default world yo YAML for easy editing Signed-off-by: microlith57 <microlith57@gmail.com>

--- desktop/server/core/paradise.js
+++ desktop/server/core/paradise.js
@@ -8,11 +8,24 @@ function Paradise () {
 
   this.reset = function () {
     // Default World
-    this.import([
-      { name: 'ghost', parent: 1, owner: 0, note: 'Well, well, hello there.' },
-      { name: 'library', parent: 1, owner: 1, note: `Hello @(vessel self "name"), and welcome to the @(cc (vessel parent "name")), the stem to an empty world. Type "@(format "learn")" to get started.` },
-      { name: 'map', parent: 0, owner: 0, note: 'A basic map', trigger: `passive`, reaction: '@(uc (vessel parent))' }
-    ])
+    _world = `
+    - name: ghost
+      parent: 1
+      owner: 0
+      note: 'Well, well, hello there.'
+    - name: library
+      parent: 1
+      owner: 0
+      note: 'Hello @(vessel self "name"), and welcome to the @(cc (vessel parent "name")), the stem to this world. Type "@(format "learn")" to get started.'
+    - name: map
+      parent: 0
+      owner: 0
+      note: 'A basic map.'
+      trigger: 'passive'
+      reaction: '@(uc (vessel parent))'
+    `
+
+    this.import(yaml.safeLoad(_world))
   }
 
   // Start
